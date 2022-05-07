d8663fc3a2c229adc9165ff3a5ebfc128cec60d7
Łukasz Dziedzic <dziedzic@alfabety.pl>
2022-05-07 20:08:50 +0200

    Initial Commit

---
+++ Shared/BadUIBezierKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadUIBezierKitApp.swift
+//  Shared
+//
+//  Created by Łukasz Dziedzic on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BadUIBezierKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
